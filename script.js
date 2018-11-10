bf4efd41ed14c307183b90d7f35ec947db2f4777
mikepenn1951 <mike.penn@gmail.com>
2018-11-10 21:02:21 +0200

    Fix hovering over Read more, add jscript for blue icon

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 /*js script*/
+function mouseOver(x) {
+    document.getElementById(x).src = "read_more_blue.png";
+}
+function mouseOutTop(x) {
+    document.getElementById(x).src = "read_more_white.png";
+}
+
+function mouseOutBot(x) {
+    document.getElementById(x).src = "read_more_yellow.png";
+}
